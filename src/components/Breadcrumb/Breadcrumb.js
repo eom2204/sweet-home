bcd8fc030fbdf999d180b2917af565bd68b931ac
Vlad <vladislav.95antonov@gmail.com>
2025-04-21 20:49:15 +0300

    fix breadcrumbs

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -3,14 +3,20 @@ import { useSelector } from "react-redux";
 import "./Breadcrumb.scss";
 
 const Breadcrumb = () => {
-  const location = useLocation(); // Get the current URL path
-  const { id } = useParams(); // Get the product id from URL params
-
-  const goods = useSelector((state) => state.goods.goods); // Access the goods list from Redux store
-
-  const pathnames = location.pathname.split("/").filter((x) => x); // Break the path into segments
-
+  const location = useLocation();
+  const { id } = useParams();
+  const goods = useSelector((state) => state.goods.goods);
+  const pathnames = location.pathname.split("/").filter(Boolean);
   const product = goods.find((item) => item.id === Number(id));
+
+  const formatText = (text) => {
+    try {
+      const decoded = decodeURIComponent(text);
+      return decoded.charAt(0).toUpperCase() + decoded.slice(1);
+    } catch {
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+  };
 
   return (
     <nav>
@@ -22,15 +28,14 @@ const Breadcrumb = () => {
         {pathnames.map((value, index) => {
           const to = `/${pathnames.slice(0, index + 1).join("/")}`;
           const isLast = index + 1 === pathnames.length;
+          const displayText = formatText(value);
 
           return (
-            <li key={to}>
-              {isLast && product ? (
-                <span>{product.name}</span>
+            <li key={`${to}-${index}`}>
+              {isLast ? (
+                <span>{product ? formatText(product.name) : displayText}</span>
               ) : (
-                <Link to={to}>
-                  {value.charAt(0).toUpperCase() + value.slice(1)}{" "}
-                </Link>
+                <Link to={to}>{displayText}</Link>
               )}
               {!isLast && " > "}
             </li>
