264982e57458dd41f3173c994082af97b3386baa
eom2204 <eom2204@gmail.com>
2025-05-03 09:22:43 +0300

    amended search block according to the new design

--- src/components/Header/SearchBlock/SearchBlock.js
+++ src/components/Header/SearchBlock/SearchBlock.js
@@ -1,10 +1,9 @@
+import * as React from 'react';
 import {useState} from 'react';
 import {useSelector} from "react-redux";
 import debounce from "lodash.debounce";
 import {useNavigate} from "react-router-dom";
 import './SearchBlock.scss';
-import Card from "../../Card/Card";
-import * as React from "react";
 
 
 const SearchBlock = () => {
@@ -37,7 +36,8 @@ const SearchBlock = () => {
         navigate(`/${category}/${name}`);
     };
 
-    const clearSearch = () => {
+    const handleSearchClose = () => {
+        setShowSearchField(false);
         setSearchTerm('');
         setFilteredItems(goods); // Reset the filtered list
     };
@@ -48,7 +48,6 @@ const SearchBlock = () => {
 
 
     return (
-
         <div className="search">
             <svg className="search__icon" width="16" height="16" viewBox="0 0 16 16" fill="none"
                  xmlns="http://www.w3.org/2000/svg">
@@ -73,6 +72,7 @@ const SearchBlock = () => {
 
             {showSearchField &&
                 (<div className='search__field'>
+
                         <div className='search__field__cards'>
                             {goods.slice(0, 3).map((product) => (
                                 <div>
@@ -96,7 +96,7 @@ const SearchBlock = () => {
                                             key={index}
                                             onClick={() => {
                                                 handleRedirect(item);
-                                                clearSearch();
+                                                handleSearchClose()
                                             }}>{item.name}
                                         </li>
                                     ))
@@ -105,25 +105,24 @@ const SearchBlock = () => {
                                 )}
                             </ul>
                         )}
+
+                        <div>
+                            <button className='search__close-btn' onClick={handleSearchClose} type="button"
+                                    aria-label="Close search">
+                                <svg width="16" height="16" viewBox="0 0 10 10" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M0.474969 0.0794034C0.583625 -0.0264678 0.759791 -0.0264678 0.868446 0.0794034L9.91842 8.89746C10.0271 9.00333 10.0271 9.17498 9.91842 9.28085L9.52495 9.66425C9.41629 9.77012 9.24013 9.77012 9.13147 9.66425L0.0814917 0.846191C-0.027164 0.740319 -0.0271638 0.568668 0.0814919 0.462797L0.474969 0.0794034Z"
+                                        fill="#A3A3A3"></path>
+                                    <path
+                                        d="M0.0815759 9.5372C-0.0270798 9.43133 -0.0270798 9.25968 0.0815759 9.15381L9.13155 0.335754C9.24021 0.229883 9.41638 0.229883 9.52503 0.335754L9.91851 0.719148C10.0272 0.825019 10.0272 0.99667 9.91851 1.10254L0.86853 9.9206C0.759875 10.0265 0.583709 10.0265 0.475053 9.9206L0.0815759 9.5372Z"
+                                        fill="#A3A3A3"></path>
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 )
             }
-
-
-            {searchTerm && (
-                <button className="search__clear-btn " onClick={clearSearch} type="button"
-                        aria-label="Clear search">
-                    <svg width="16" height="16" viewBox="0 0 10 10" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M0.474969 0.0794034C0.583625 -0.0264678 0.759791 -0.0264678 0.868446 0.0794034L9.91842 8.89746C10.0271 9.00333 10.0271 9.17498 9.91842 9.28085L9.52495 9.66425C9.41629 9.77012 9.24013 9.77012 9.13147 9.66425L0.0814917 0.846191C-0.027164 0.740319 -0.0271638 0.568668 0.0814919 0.462797L0.474969 0.0794034Z"
-                            fill="#A3A3A3"></path>
-                        <path
-                            d="M0.0815759 9.5372C-0.0270798 9.43133 -0.0270798 9.25968 0.0815759 9.15381L9.13155 0.335754C9.24021 0.229883 9.41638 0.229883 9.52503 0.335754L9.91851 0.719148C10.0272 0.825019 10.0272 0.99667 9.91851 1.10254L0.86853 9.9206C0.759875 10.0265 0.583709 10.0265 0.475053 9.9206L0.0815759 9.5372Z"
-                            fill="#A3A3A3"></path>
-                    </svg>
-                </button>
-            )}
         </div>
     )
 }
